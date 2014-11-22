5e285143374e9ef75e89a71692655ec3d662bc8e
Rey Tolentino <rtole@guidance.com>
2014-11-21 17:02:45 -0800

    Revert "re extension Subscription update"

--- app/code/local/AW/All/Model/Feed/Updates.php
+++ app/code/local/AW/All/Model/Feed/Updates.php
@@ -1,29 +1,27 @@
 <?php
 /**
-* aheadWorks Co.
+ * aheadWorks Co.
  *
  * NOTICE OF LICENSE
  *
  * This source file is subject to the EULA
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
- * http://ecommerce.aheadworks.com/AW-LICENSE-ENTERPRISE.txt
+ * http://ecommerce.aheadworks.com/AW-LICENSE.txt
  *
  * =================================================================
  *                 MAGENTO EDITION USAGE NOTICE
  * =================================================================
- * This package designed for Magento ENTERPRISE edition
- * aheadWorks does not guarantee correct work of this extension
- * on any other Magento edition except Magento ENTERPRISE edition.
- * aheadWorks does not provide extension support in case of
- * incorrect edition usage.
+ * This software is designed to work with Magento enterprise edition and
+ * its use on an edition other than specified is prohibited. aheadWorks does not
+ * provide extension support in case of incorrect edition use.
  * =================================================================
  *
  * @category   AW
- * @package    AW_All
- * @version    2.2.1
+ * @package    AW_Sarp2
+ * @version    2.0.0
  * @copyright  Copyright (c) 2010-2012 aheadWorks Co. (http://www.aheadworks.com)
- * @license    http://ecommerce.aheadworks.com/AW-LICENSE-ENTERPRISE.txt
+ * @license    http://ecommerce.aheadworks.com/AW-LICENSE.txt
  */
 
 class AW_All_Model_Feed_Updates extends AW_All_Model_Feed_Abstract

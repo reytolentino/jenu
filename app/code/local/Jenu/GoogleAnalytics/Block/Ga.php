195f2e7087493bff9b6cce1dac8ff796b8e23de7
Rey Tolentino <rey@jenu.com>
2015-02-13 15:41:43 -0800

    re ga update

--- app/code/local/Jenu/GoogleAnalytics/Block/Ga.php
+++ app/code/local/Jenu/GoogleAnalytics/Block/Ga.php
@@ -11,24 +11,11 @@ class Jenu_GoogleAnalytics_Block_Ga extends Mage_GoogleAnalytics_Block_Ga
 
     protected function _getPageTrackingCodeUniversal($accountId)
     {
-        if(Mage::getSingleton('customer/session')->isLoggedIn()) {
-            $customerData = Mage::getSingleton('customer/session')->getCustomer();
-            $loggedInId = $customerData->getId();
-        }
+        $visitorData = Mage::getSingleton('core/session')->getVisitorData();
+        $loggedInId = $visitorData['visitor_id'];
         return "
-        var prefix = ['abc', 'def', 'ghi'],
-          middle = ['123', '456', '789'],
-          suffix = ['rst', 'uvw', 'xyz'],
-          random = function() {
-              return Math.floor(Math.random() * 3);
-          };
         var customUserId;
         customUserId = '{$loggedInId}';
-        if (customUserId) {
-        customUserId = '{$loggedInId}';
-        } else {
-        customUserId = prefix[random()] + '-' + middle[random()] + '-' + suffix[random()];
-        }
         ga('create', '{$this->jsQuoteEscape($accountId)}', {'userId': customUserId});
         ga('create', '{$this->jsQuoteEscape($accountId)}', 'auto');
         " . $this->_getAnonymizationCode() . "

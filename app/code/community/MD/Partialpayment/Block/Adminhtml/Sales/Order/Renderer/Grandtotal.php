68bd2a50e44091f76e7be20d1d7b0c5a2d523ff2
magedelight <support@magedelight.com>
2015-04-03 11:48:07 +0530

    Add Total Paid amount below G.T. Purchased in Sales Order Grid

---
+++ app/code/community/MD/Partialpayment/Block/Adminhtml/Sales/Order/Renderer/Grandtotal.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Magedelight
+ * Copyright (C) 2014 Magedelight <info@magedelight.com>
+ *
+ * NOTICE OF LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://opensource.org/licenses/gpl-3.0.html.
+ *
+ * @category MD
+ * @package MD_Partialpayment
+ * @copyright Copyright (c) 2014 Mage Delight (http://www.magedelight.com/)
+ * @license http://opensource.org/licenses/gpl-3.0.html GNU General Public License,version 3 (GPL-3.0)
+ * @author Magedelight <info@magedelight.com>
+ */
+class MD_Partialpayment_Block_Adminhtml_Sales_Order_Renderer_Grandtotal extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract {
+
+    public function render(Varien_Object $row) {
+	$grandTotal = $row->getData('grand_total');
+	$totalPaid  = $row->getData('total_paid');
+	
+	if ($totalPaid > 0) {
+	    return Mage::helper('core')->currency($grandTotal, true, false) . '<br/>' . Mage::helper('core')->currency($totalPaid, true, false);
+	} else {
+	    return Mage::helper('core')->currency($grandTotal, true, false);
+	}
+    }
+
+}

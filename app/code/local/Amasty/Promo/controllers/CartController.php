db344cfc79868a1bb5209db9d763790f1685fba9
Rey Tolentino <rey@jenu.com>
2015-07-07 15:09:29 -0700

    re Amasty install

--- app/code/local/Amasty/Promo/controllers/CartController.php
+++ app/code/local/Amasty/Promo/controllers/CartController.php
@@ -1,19 +1,67 @@
 <?php
 /**
-* @copyright Amasty.
-*/ 
+ * @author Amasty Team
+ * @copyright Copyright (c) 2015 Amasty (https://www.amasty.com)
+ * @package Amasty_Promo
+ */
 class Amasty_Promo_CartController extends Mage_Core_Controller_Front_Action
 {
-    public function indexAction()
+    public function updateAction()
     {
-    }
+        $productId = $this->getRequest()->getParam('product_id');
 
-    public function restoreAction()
-    {
-        $session = Mage::getSingleton('checkout/session');
-        $session->setAmpromoDeletedItems(null);
-        $session->setAmpromoMessages(null);
+        $product = Mage::getModel('catalog/product')->load($productId);
 
-        $this->_redirect('checkout/cart/index');
+        if ($product->getId())
+        {
+            $limits  = Mage::getSingleton('ampromo/registry')->getLimits();
+
+            $sku = $product->getSku();
+
+            $addAllRule = isset($limits[$sku]) && $limits[$sku] > 0;
+            $addOneRule = false;
+            if (!$addAllRule)
+            {
+                foreach ($limits['_groups'] as $ruleId => $rule)
+                {
+                    if (in_array($sku, $rule['sku']))
+                    {
+                        $addOneRule = $ruleId;
+                    }
+                }
+
+            }
+            if ($addAllRule || $addOneRule)
+            {
+                $super = $this->getRequest()->getParam('super_attributes');
+                $options = $this->getRequest()->getParam('options');
+                $bundleOptions = $this->getRequest()->getParam('bundle_option');
+
+				/* To compatibility amgiftcard module */
+				$amgiftcardValues = array();
+				if($product->getTypeId() == 'amgiftcard') {
+					$amgiftcardFields = array_keys(Mage::helper('amgiftcard')->getAmGiftCardFields());
+					foreach($amgiftcardFields as $amgiftcardField) {
+						if($this->getRequest()->getParam($amgiftcardField)) {
+							$amgiftcardValues[$amgiftcardField] = $this->getRequest()->getParam($amgiftcardField);
+						}
+					}
+				}
+
+                Mage::helper('ampromo')->addProduct($product, $super, $options, $bundleOptions, $addOneRule, $amgiftcardValues);
+            }
+        }
+
+        $referer = $this->getRequest()->getPost('referer');
+
+        $referer = Mage::helper('core')->urlDecode($referer);
+
+        $urlModel = Mage::getModel('core/url');
+
+        if (method_exists($urlModel, 'getRebuiltUrl')) { // Fix for old versions
+            $referer = $urlModel->getRebuiltUrl($referer);
+        }
+
+        $this->getResponse()->setRedirect($referer);
     }
 }

5f7cb3e63d5561be6b3f670d29da85cdbb3488c9
Rey Tolentino <reytolentino3@gmail.com>
2016-01-20 13:00:14 -0800

    re update

--- app/code/community/MD/Partialpayment/Block/Adminhtml/Sales/Order/Grid.php
+++ app/code/community/MD/Partialpayment/Block/Adminhtml/Sales/Order/Grid.php
@@ -47,7 +47,8 @@ class MD_Partialpayment_Block_Adminhtml_Sales_Order_Grid extends AW_Sarp_Block_A
 
     protected function _prepareCollection() {
 	$collection = Mage::getResourceModel($this->_getCollectionClass());
-
+	$collection->getSelect()->joinLeft('sales_flat_order', 'main_table.entity_id = sales_flat_order.entity_id','tax_amount');
+	$collection->addFilterToMap('created_at','main_table.created_at');
 	$this->setCollection($collection);
 	return parent::_prepareCollection();
     }

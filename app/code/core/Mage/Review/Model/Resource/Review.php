06bba221423bacf160458c6da74b800f7c4ee1f8
Rey Tolentino <reytolentino3@gmail.com>
2016-02-12 11:07:41 -0800

    re test update

--- app/code/core/Mage/Review/Model/Resource/Review.php
+++ app/code/core/Mage/Review/Model/Resource/Review.php
@@ -153,6 +153,7 @@ class Mage_Review_Model_Resource_Review extends Mage_Core_Model_Resource_Db_Abst
             'title'     => $object->getTitle(),
             'detail'    => $object->getDetail(),
             'nickname'  => $object->getNickname(),
+            'email'     => $object->getEmail(),
         );
         $select = $adapter->select()
             ->from($this->_reviewDetailTable, 'detail_id')

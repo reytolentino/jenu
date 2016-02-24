31e79a7e1f1d9f5da70357b7de8892b0c9eadbd7
Rey Tolentino <reytolentino3@gmail.com>
2016-02-23 23:13:14 -0800

    re update

--- app/code/local/Jenu/ReviewForm/sql/jenu_reviewform_setup/install-1.0.0.php
+++ app/code/local/Jenu/ReviewForm/sql/jenu_reviewform_setup/install-1.0.0.php
@@ -11,6 +11,7 @@ $installer->getConnection()
         'length'    => 60,
         'comment'   => 'Email Address'
     ));
+$installer->getConnection()
     ->addColumn($installer->getTable('review/review_detail'),'products', array(
         'type'      => Varien_Db_Ddl_Table::TYPE_TEXT,
         'nullable'  => true,

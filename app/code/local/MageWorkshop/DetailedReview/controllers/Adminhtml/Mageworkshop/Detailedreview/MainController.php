2b39c804096987689dd2da5ee9f35c7df58b3263
Rey Tolentino <reytolentino3@gmail.com>
2016-06-07 15:36:10 -0700

    re review detailed

---
+++ app/code/local/MageWorkshop/DetailedReview/controllers/Adminhtml/Mageworkshop/Detailedreview/MainController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * MageWorkshop
+ * Copyright (C) 2016 MageWorkshop <mageworkshophq@gmail.com>
+ *
+ * @category   MageWorkshop
+ * @package    MageWorkshop_DetailedReview
+ * @copyright  Copyright (c) 2016 MageWorkshop Co. (http://mage-workshop.com)
+ * @license    http://opensource.org/licenses/gpl-3.0.html GNU General Public License, version 3 (GPL-3.0)
+ * @author     MageWorkshop <mageworkshophq@gmail.com>
+ */
+class MageWorkshop_DetailedReview_Adminhtml_Mageworkshop_Detailedreview_MainController extends Mage_Adminhtml_Controller_Action
+{
+    /**
+     * Reindex sorting attribute
+     */
+    public function reindexAction()
+    {
+        $helper = Mage::helper('detailedreview');
+        try {
+            if (Mage::getStoreConfig('detailedreview/settings/deny_change_polarity_group')) {
+                Mage::getModel('detailedreview/sort')->refreshAllIndices();
+            } else {
+                throw new Exception('Reindex disabled, please check config Detailed Review -> Settings -> Deny Administrator to change Popularity');
+            }
+
+            Mage::getSingleton('adminhtml/session')->addSuccess($helper->__('The data were indexed'));
+            $this->_redirectReferer();
+        } catch (Exception $e) {
+            $this->_redirectReferer();
+            Mage::getSingleton('adminhtml/session')->addError($helper->__('Reindex was failed. %s', $e->getMessage()));
+        }
+    }
+
+    protected function _isAllowed()
+    {
+        return true;
+    }
+}
